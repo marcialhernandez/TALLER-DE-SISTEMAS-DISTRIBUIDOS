878cd3d4473f5b1f2708d6b818e3dc6d107e8f9d
eterna_Ubuntu <marcialhs@ymail.com>
2014-09-29 18:15:51 -0300

    copia de cliente pa Felmer

---
+++ Cliente-RMI (2)/src/cliente/Cliente.java
@@ -0,0 +1,74 @@
+package cliente;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import rmi.ConexionCliente;
+import rmi_interface.Interface;
+
+/**
+ *
+ * @author Daniel Wladdimiro Cottet
+ * @title Taller de sistemas distribuidos - Clase 1
+ */
+public class Cliente {
+
+    public static int Puerto = 2014;                                 //Número del puerto que está alojado el servidor
+    public static String IPServer = "10.42.0.1";                    //Dirección IP del servidor, la cual podría utilizarse por defecto el localhost
+    public static String nombreReferenciaRemota = "Ejemplo-RMI"; // Nombre del objeto subido
+
+    public static void main(String[] args) {
+
+        Interface objetoRemoto; //Se crea un nuevo objeto llamado objetoRemoto
+
+        //Se instancia el objeto que conecta con el servidor
+        ConexionCliente conexion = new ConexionCliente();
+        try {
+            //Se conecta con el servidor
+            if (conexion.iniciarRegistro(IPServer, Puerto, nombreReferenciaRemota)) {
+
+                //Se obtiene la referencia al objeto remoto
+                objetoRemoto = conexion.getServidor();
+
+                int opcion = 0;
+                while (opcion != 3) {
+
+                    //Escoger alguna opción del menú
+                    System.out.println("Menú RMI\n1. Ingresar un usuario al servidor\n2. Ver usuarios del servidor\n3. Salir");
+                    Scanner sc = new Scanner(System.in);
+                    opcion = Integer.parseInt(sc.next());
+
+                    if (opcion == 1) {
+
+                        System.out.println("Ingrese el nombre del usuario: ");
+                        sc = new Scanner(System.in);
+                        String usuario = sc.next();
+
+                        //Llama a un método del objeto remoto, y se le ingresa un parámetro a éste método
+                        boolean ingreso = objetoRemoto.ingresarUsuario(usuario);
+                        if (ingreso) {
+                            System.out.println("¡Felicitaciones, ha sido agregado el usuario!");
+                        } else {
+                            System.out.println("Lamentablemente no ha sido ingresado el usuario, pruebe con otro nombre...");
+                        }
+
+                    } else if (opcion == 2) {
+                        //Llama a un método del objeto remoto
+                        ArrayList<String> usuarios = objetoRemoto.verUsuarios();
+
+                        for (String usuario : usuarios) {
+                            System.out.println("Usuario: " + usuario);
+                        }
+
+                    } else if (opcion != 3) {
+                        System.out.println("Ingrese un número válido por favor...");
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido un error");
+        }
+
+        System.exit(0);
+    }
+}
